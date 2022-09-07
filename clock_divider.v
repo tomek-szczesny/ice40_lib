4decb0c210bf0b8dfaab7115caacd450f88b1ab7
Tomek Szczęsny <mctom@tlen.pl>
2022-09-07 16:41:51 +0200

    clkdiv testbench + fixes

--- clock_divider.v
+++ clock_divider.v
@@ -3,7 +3,7 @@
 // by Tomek Szczesny 2022
 //
 // clkdiv divides the frequency of input clock signal by "divider".
-// "divider" is rounded up to nearest even integer.
+// "divider" is rounded down to the nearest even integer.
 // The output signal duty cycle is always 50%.
 //
 //            +------------------+
@@ -13,7 +13,7 @@
 //            +------------------+
 //
 // Parameters:
-// divider - a clock divider
+// divider - a clock divider. Rounded down to the nearest even integer.
 //
 // Ports: 
 // in	- Input
@@ -21,16 +21,16 @@
 //
 module clkdiv(
 	input wire in,
-	output reg out
+	output reg out = 0
 );
-parameter divider = 1000;
+parameter divider = 2;
 
-reg [$clog2((divider/2)+1):0] clkdiv;
+reg [$clog2((divider/2)+1):0] clkdiv = 0;
 
 always@(posedge in)
 begin
 	if (clkdiv >= (divider/2)) begin
-		clkdiv <= 0;
+		clkdiv <= 1;
 		out <= ~out;
 	end else begin
 		clkdiv <= clkdiv + 1;
---
+++ tb_clock_divider.v
@@ -0,0 +1,32 @@
+`include "clock_divider.v"
+
+module top(
+);
+
+reg  clk_in = 0;
+wire clk_out;
+parameter clk_div = 6;
+
+initial
+begin
+$display ("Clock divider: %d", clk_div);
+#300 $finish;
+end
+
+
+always
+begin
+	#5 clk_in <= ~clk_in;
+	#5 $display ("in: %d, out: %d", clk_in, clk_out);
+end
+
+
+clkdiv #(
+	.divider(clk_div))
+	MUT (
+	.in(clk_in),
+	.out(clk_out));
+
+
+endmodule
+
