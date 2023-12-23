1f9515064f5fc4d61b04e7736aa17a0912922887
Tomek Szczęsny <mctom@tlen.pl>
2023-12-23 15:37:02 +0100

    DDR Video Test

--- video_test.v
+++ video_test.v
@@ -6,6 +6,8 @@
 // enable pin if the output buffer is full.
 //
 // Internal state may be reset to synchronize with video transmitter.
+// 
+// video_test_ddr can be found below, it generates 2 pixels at the same time.
 //
 //              +----------+                                + - - - - - 
 //      clk --->|          |---> clk_o - - - - - - - clk - >|    
@@ -108,7 +110,7 @@ begin
 				end
 			end
 			2'd2: begin
-				if (rom_data[vr[2:0]]) begin
+				if (rom_data[7-hr[2:0]]) begin
 					rd <= rmax;
 					gd <= gmax;
 					bd <= bmax;
@@ -130,14 +132,149 @@ end
 rom #(
 	.m(32),
 	.n(8),
-	.data({8'd00, 8'd56,8'd08,8'd24,8'd08,8'd56,8'd00,8'd56,8'd40,8'd40,8'd00,8'd04,8'd62,8'd04,8'd00,8'd56,8'd40,8'd56,8'd00,8'd56,8'd08,8'd24,8'd08,8'd56,8'd00,8'd00,8'd00,8'd00,8'd00,8'd00,8'd00,8'd00}),
+	.data({8'h00,8'h00,8'h00,8'hfb,8'haa,8'h8b,8'h00,8'h00,8'h00,8'h10,8'h38,8'h93,8'h12,8'h93,8'h00,8'h00,8'h00,8'h00,8'h00,8'hbe,8'haa,8'ha2,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00}),
 	.content_size(32)
 ) vt_rom (
-	.clk(gclk),
-	.address(hr[5:0]),
+	.clk(~gclk),
+	.address({hr[4:3],vr[2:0]}),
 	.data_o(rom_data)
 );
 
 endmodule
 
+//
+// video_test_ddr
+// Generates two pixels at the same time,
+// which is useful with ddr-based drivers.
+// all parameters and io remain the same, except 
+// outputs "xd" are replaced with "xdo" and "xde",
+// for odd and even pixel data, respectively.
+// Notice that pixel positions starts with "0" which is even.
+
+module video_test_ddr(
+	input wire clk,
+	input wire cke,
+	input wire[1:0] pattern,
+	input wire rst,
+	output wire[r-1:0] rdo,
+	output wire[g-1:0] gdo,
+	output wire[b-1:0] bdo,
+	output wire[r-1:0] rde,
+	output wire[g-1:0] gde,
+	output wire[b-1:0] bde,
+	output wire clk_o
+);
+parameter v = 480;
+parameter h = 640;
+parameter r = 8;
+parameter g = 8;
+parameter b = 8;
+localparam rmax = 2**r-1;
+localparam gmax = 2**g-1;
+localparam bmax = 2**b-1;
+
+// Coordinate counters
+reg [$clog2(v):0] vr = 0;
+reg [$clog2(h):0] hr = 0;
+wire [$clog2(h):0] hrp1;
+assign hrp1 = hr | 1'b1;	// hr plus one; hr is always even
+
+// ROM data wire
+wire [7:0] rom_data;
+
+// Input clock gate
+wire gclk;
+assign clk_o = gclk;
+clkgate vt_ckg (
+	.in(clk),
+	.gate(cke),
+	.out(gclk)
+);
+
+always@(posedge gclk)
+begin
+	// coordinates counters
+	if (rst) begin
+		vr <= 0;
+		hr <= 0;
+	end else begin
+		if (hr == h-2) begin
+			hr <= 0;
+			vr <= (vr == v-1) ? 0 : vr + 1;
+		end else begin
+			hr <= hr+2;
+		end
+	end
+
+	// Actual pattern generation
+		case (pattern)
+			2'd0: begin
+				rde <= 0;
+				gde <= gmax;
+				bde <= 0;
+				rdo <= 0;
+				gdo <= gmax;
+				bdo <= 0;
+			end
+			2'd1: begin
+				if (vr[0] ^ hr[0]) begin
+					rde <= 0;
+					gde <= 0;
+					bde <= 0;
+					rdo <= rmax;
+					gdo <= gmax;
+					bdo <= bmax;
+				end else begin
+					rde <= rmax;
+					gde <= gmax;
+					bde <= bmax;
+					rdo <= 0;
+					gdo <= 0;
+					bdo <= 0;
+				end
+			end
+			2'd2: begin
+				if (rom_data[7-hr[2:0]]) begin
+					rde <= rmax;
+					gde <= gmax;
+					bde <= bmax;
+				end else begin
+					rde <= 0;
+					gde <= 0;
+					bde <= 0;
+				end
+				if (rom_data[7-hrp1[2:0]]) begin
+					rdo <= rmax;
+					gdo <= gmax;
+					bdo <= bmax;
+				end else begin
+					rdo <= 0;
+					gdo <= 0;
+					bdo <= 0;
+				end
+			end
+			2'd3: begin
+				rde <= hr[r-1:0] ^ vr[r-1:0];
+				gde <= hr[g:1] ^ vr[g-1:0];
+				bde <= hr[b-1:0] ^ vr[b:1];
+				rdo <= hrp1[r-1:0] ^ vr[r-1:0];
+				gdo <= hrp1[g:1] ^ vr[g-1:0];
+				bdo <= hrp1[b-1:0] ^ vr[b:1];
+			end
+		endcase
+end
+
+// ROM with "mctom" bitmap
+rom #(
+	.m(32),
+	.n(8),
+	.data({8'h00,8'h00,8'h00,8'hfb,8'haa,8'h8b,8'h00,8'h00,8'h00,8'h10,8'h38,8'h93,8'h12,8'h93,8'h00,8'h00,8'h00,8'h00,8'h00,8'hbe,8'haa,8'ha2,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00,8'h00}),
+	.content_size(32)
+) vt_rom (
+	.clk(~gclk),
+	.address({hr[4:3],vr[2:0]}),
+	.data_o(rom_data)
+);
+
+endmodule
 `endif
