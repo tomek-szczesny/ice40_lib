ce4ff5f73c2f71c744b5e4bef9bee80f8ef71e15
Tomek Szczęsny <mctom@tlen.pl>
2024-08-11 14:49:39 +0200

    7-bit PR counter

--- counters.v
+++ counters.v
@@ -30,7 +30,7 @@ begin
 end
 
 endmodule
-
+//
 ///////
 module ctr_pr5(
 	input wire clk,
@@ -56,6 +56,38 @@ always @ (posedge clk)
 begin
 	if (inc) begin
 		msb <= {msb[0], out[n-1]};
+		out [n-1:1] <= out[n-2:0];
+		out[0] <= lo;
+	end
+end
+
+endmodule
+
+///////
+module ctr_pr7(
+	input wire clk,
+	input wire inc,
+	output reg [n-1:0] out = 0);
+
+localparam n = 7;
+localparam lut_data = 16'b1100001101001011;
+
+wire lo;
+reg msb = 1'b0;
+
+SB_LUT4 lut (
+	.O(lo),
+	.I0(out[0]),
+	.I1(out[3]),
+	.I2(out[6]),
+	.I3(msb)
+);
+defparam lut.LUT_INIT = lut_data;
+
+always @ (posedge clk)
+begin
+	if (inc) begin
+		msb <= out[n-1];
 		out [n-1:1] <= out[n-2:0];
 		out[0] <= lo;
 	end
--- tb_ctr_pr.v
+++ tb_ctr_pr.v
@@ -7,16 +7,17 @@ module top(
 reg clk_in = 0;
 wire [3:0] out4;
 wire [4:0] out5;
+wire [6:0] out7;
 
 initial
 begin
-#500	$finish;
+#2560	$finish;
 end
 
 always
 begin
 	#5 clk_in <= ~clk_in;
-	if (~clk_in) $display ("clk: %b, out4: %b (%d)\t out5: %b (%d)", clk_in, out4, out4, out5, out5);
+	if (~clk_in) $display ("clk: %b, out4: %b (%d)\t out5: %b (%d)\tout7: %b (%d)", clk_in, out4, out4, out5, out5, out7, out7);
 end
 
 ctr_pr4 MUT4 (
@@ -28,6 +29,11 @@ ctr_pr5 MUT5 (
 	.clk(clk_in),
 	.inc(1'b1),
 	.out(out5));
+
+ctr_pr7 MUT7 (
+	.clk(clk_in),
+	.inc(1'b1),
+	.out(out7));
 
 endmodule
 
