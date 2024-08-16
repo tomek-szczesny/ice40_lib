1de12a60a8d18484e1800516ec2708b42d87af58
Tomek Szczęsny <mctom@tlen.pl>
2024-08-16 19:42:10 +0200

    TNG prdiv

--- tools/prdiv.cpp
+++ tools/prdiv.cpp
@@ -8,20 +8,336 @@
 //
 
 #include <bitset>
+#include <chrono>
 #include <cmath>
+#include <stdlib.h>     /* srand, rand */
 #include <iostream>
 #include <string>
+#include <time.h>	// For srand
 #include <vector>
 	
-int b = 0;		// Counter bitness (based on p)
 int p = 0;		// Counter period  (cmd line arg)
+int b = 0;		// Counter bitness (based on p)
 int sx = 3;		// Max extra bits  (cmd line arg)
+int x = 0;		// Extra bits
 
-//const int t = b + x - 1;
-int max = pow(2,b);
+int fact(int in)
+{
+	int o = 1;
+	int i;
+	for (i=2;i<=in;i++)
+	{
+		o *= i;
+	}
+	return o;
+}
 
-std::vector<bool> results(20480);
-int x = 0;	// Extra bits
+class lut {
+	// A class representing a 4-bit LUT
+	// It supports "don't care" states to some extent
+	
+	private:
+		std::vector<bool> lut_d;//(16, 0);
+		std::vector<bool> lut_x;//(16, 1);
+	
+	public:
+	lut()
+	{
+		int i;
+		for (i=0;i<16;i++)
+		{
+			lut_d.push_back(0);
+			lut_x.push_back(1);
+		}
+	}
+	// reading
+	bool operator[](int i) const
+	{
+		return lut_d[i];
+	}
+	//writing
+	// Returns zero when trying to overwrite an opposite value
+	bool set (int i, bool val)
+	{
+		if (val != lut_d[i] && !lut_x[i]) return 0;
+		lut_d[i] = val;
+		lut_x[i] = 0;
+		return 1;
+	}
+	void unset (int i)
+	{
+		lut_d[i] = 0;
+		lut_x[i] = 1;
+	}
+	void clear()
+	{
+		lut_x.assign(16, 1);
+		lut_d.assign(16, 0);
+		/*
+		int i;
+		for (i=15;i>=0;i--)
+		{
+			lut_x[i] = 1;
+			lut_d[i] = 0;
+		}
+		*/
+	}
+
+	std::string str()
+	{
+		int i;
+		std::string ret;
+		for (i=15;i>=0;i--)
+		{
+			if (lut_x[i]) ret += "x";
+			else ret += (lut_d[i] ? "1" : "0");
+		}
+		return ret;
+	}
+	// Evaluates the LUT output for a given set of inputs,
+	// represented by a binary number 0000 - 1111.
+	bool eval(int d)
+	{
+		return lut_d[d];
+	}
+};
+
+class comb {
+	// A class representing a combination "k of n"
+	// without repetitions
+	// Used for selecting LUT inputs
+	// It can also return "k of n" bits from an integer.
+
+	private:
+		int c = 0;
+		int k = 0;
+		int n = 0;
+
+	public:
+
+	comb(int k, int n)
+	{
+		this->k = k;
+		this->n = n;
+		//c = int(std::pow(2,k)-1) << (n-k);	// The first combination
+		//c = 0;
+	}
+	// Set the next combination
+	// Returns zero if rolled over.
+	bool next()
+	{
+		int i;
+		while (1)
+		{
+			this->c++;
+			this->c &= int(pow(2,n)-1);
+			if (c == 0) 
+			{
+				this->next();
+				return 0;
+			}
+			if (this->check()) return 1;
+		}
+	}
+	// Set an internal state
+	// If the state is invalid, it tries to reach the next valid one.
+	// Returns zero if rolled over in the process.
+	bool set(int i)
+	{
+		c = i;
+		if (this->check()) return 1;
+		else return this->next();
+	}
+	// Checks if its internal state is valid.
+	// Mostly for internal use.
+	bool check()
+	{
+		int i;
+		int o = 0;
+		for (i=0; i<n; i++)
+		{
+			o += (c >> i) & 1;
+		}
+		return (o == k);
+	}
+	// Composes a new int from selected bits of the in.
+	int map(int in)
+	{
+		int ret = 0;
+		int i;
+		int k = 0;
+		for (i=0;i<n;i++)
+		{
+			if ((c >> i) & 1) 
+			{
+				ret += ((in >> i) & 1) << i-k;
+			}
+			else k++;
+		}
+		return ret;
+	}
+	// get current state as a string
+	std::string str()
+	{
+		int i;
+		std::string ret;
+		for (i=n-1;i>=0;i--)
+		{
+			ret += ((c >> i) & 1 ? "1" : "0");
+		}
+		return ret;
+	}
+	// get current state as a vector of numbers
+	std::vector<int> vec()
+	{
+		int i;
+		std::vector<int> ret;
+		for (i=0;i<n;i++)
+		{
+			if ((c >> i) & 1) ret.push_back(i);
+		}
+		return ret;
+	}
+
+
+};
+
+class vari {
+	// Helps generating variants without repetitions
+	// Operates on a range of numbers from l to h.
+	// selects k of n items, where n = (h-l+1);
+	
+	private:
+	int k; int n;
+	std::vector<int> s;		// internal state
+	std::vector<int> range;
+	std::vector<int> result;
+	
+	// Generates the next internal state.
+	bool next_state(int num = 1000)
+	{
+		int i;
+		if (num > k-1) num = k-1;
+		s[num] +=1;
+		for (i=num; i>=0; i--)
+		{
+			if (s[i] > n-i-1)
+			{
+				s[i] = 0;
+				if (i == 0) return 0;
+				s[i-1] += 1;
+			}
+		}
+		for (i=num+1; i<k; i++)
+		{
+			s[i] = 0;
+		}
+		return 1;
+	}
+
+	public:
+		
+	vari(int l, int h, int k)
+	{
+		int i;
+		this->n = h-l+1;
+		this->k = k;
+		for (i=l; i<=h; i++)
+		{
+			range.push_back(i);
+		}
+
+		std::vector<int> r = range;	// A disposable copy
+		result.clear();
+		
+		for (i=0;i<k;i++)
+		{
+			s.push_back(0);
+			result.push_back(r[s[i]]);
+			r.erase(r.begin() + s[i]);
+		}
+
+	}
+	
+
+	// Generates the next variance
+	// Returns zero if rolled back to the first one.
+	bool next(int num = 1000)
+	{
+		if (this->next_state(num) == 0) return 0;
+
+		std::vector<int> r = range;	// A disposable copy
+		result.clear();
+		
+		int i; for (i=0;i<k;i++)
+		{
+			result.push_back(r[s[i]]);
+			r.erase(r.begin() + s[i]);
+		}
+		return 1;
+	}
+
+	// Generates the next variance, but randomizes the state.
+	// Always returns 1.
+	bool random()
+	{
+		int i;
+		for (i=0; i<k; i++)
+		{
+			s[i] = rand() % (n-i);
+		}
+		this->next();
+		return 1;
+	}
+		
+
+	// Returns a current result vector
+	std::vector<int> get() 
+	{
+		return result;
+	}
+	// Returns a number of cases to go through
+	long int cases()
+	{
+		long int ret = 1;
+		int i;
+		for (i=(n-k+1); i<=n; i++)
+		{
+			ret *= i;
+		}
+		return ret;
+	}
+};
+
+
+
+// Return the state number in which emplacement failed.
+// Returns 0 if succeeded.
+int fill_luts (std::vector<lut> & luts, std::vector<int> & states, std::vector<comb> & configs)
+{
+	int i, j;
+	int p2 = (p+1)/2;
+
+	for (i=0; i<b+x; i++) luts[i].clear();
+
+	for (j=1; j<(p2)+1; j++)		// For each state
+	{
+		for (i=0; i<b+x; i++)		// For each bit
+		{
+			if (luts[i].set(configs[i].map(states[j-1]), states[j] >> i & 1) == 0)
+				return j + (i << 16);
+			if ((j+p2) < states.size()) {
+				if (luts[i].set(configs[i].map(states[j-1+p2]), states[j+p2] >> i & 1) == 0)
+					return j + (i << 16);
+			}
+			// Special case - machine state loop back
+			else if (luts[i].set(configs[i].map(states[p-1]), states[0] >> i & 1) == 0)
+				return j + (i << 16);
+		}
+	}
+	return 0;
+}
+
 
 inline std::string bincout (int in, int w = 32)
 {
@@ -57,220 +373,152 @@ std::vector<std::string> parseconfig_s(int config)
 	return rets;
 }
 
-void printmodule(int reactor1, int reactor2, int config1, int config2, int mode)
+bool mass_next(std::vector<comb> & c)
 {
+	for (comb & i : c)
+	{
+		if (i.next()) return 1;
+	}
+	return 0;
+}
+
+void printmodule(std::vector<lut> & luts, std::vector<int> & states, std::vector<comb> & configs)
+{
+	std::cout << std::nounitbuf << "\n";
+	std::cout << "// Auto generated divider / pseudo-random counter.\n";
+	//std::cout << "// It took " << t << " seconds to find this configuration.\n";
+	std::cout << "// States:";
+	for (auto i : states) std::cout << " " << i;
 	std::cout << "\n";
-	std::cout << "module div_pr" << p << "(input wire clk, input wire rst, output wire out);\n";
+	std::cout << "module div_pr" << p << "(input wire clk, input wire rst, output reg [" << b+x-1 << ":0] out);\n";
 
-	std::cout << "localparam lut1_data = 16'b" << bincout(reactor1,16) << ";\n";
-	if (config2) std::cout << "localparam lut2_data = 16'b" << bincout(reactor2,16) << ";\n";
+	int j = 0;
+	for (auto i : luts)
+	{
+		std::cout << "localparam lut" << j << "_data = 16'b" << i.str() << ";\n";
+		j++;
+	}
 
-	std::cout << "wire lo1, lo2;\n";
-	std::cout << "reg [" << x+b-1 << ":0] s = 0;\n";
-	std::cout << "assign out = s[" << x+b-1 << "];\n";
+	std::cout << "wire [" << luts.size()-1 << ":0] lo;\n";
 
-	std::vector<std::string> pcs = parseconfig_s(config1);
-	std::cout << "SB_LUT4 lut1 (.O(lo1), .I0(" << pcs.at(0);
-	std::cout << "), .I1(" << pcs.at(1) << "), .I2(";
-	std::cout << pcs.at(2) << "), .I3(" << pcs.at(3) << "));\n";
-	std::cout << "defparam lut1.LUT_INIT = lut1_data;\n";
-
-	if (mode) {
-		pcs = parseconfig_s(config2);
-		std::cout << "SB_LUT4 lut2 (.O(lo2), .I0(" << pcs.at(0);
-		std::cout << "), .I1(" << pcs.at(1) << "), .I2(";
-		std::cout << pcs.at(2) << "), .I3(" << pcs.at(3) << "));\n";
-		std::cout << "defparam lut2.LUT_INIT = lut2_data;\n";
+	std::vector<int> pcs;
+	for (j=0;j<luts.size();j++)
+	{
+		pcs = configs[j].vec();
+		std::cout << "SB_LUT4 lut" << j << " (.O(lo[" << j << "]), ";
+		std::cout << ".I0(out[" << pcs.at(0) << "]), ";
+		std::cout << ".I1(out[" << pcs.at(1) << "]), ";
+		std::cout << ".I2(out[" << pcs.at(2) << "]), ";
+		std::cout << ".I3(out[" << pcs.at(3) << "]));\n";
+		std::cout << "defparam lut" << j << ".LUT_INIT = lut" << j << "_data;\n";
 	}
 
 	std::cout << "always @ (posedge clk) begin\n";	
-	std::cout << "\tif (rst) s <= 0;\n";	
-	std::cout << "\telse begin\n";	
-	std::cout << "\t\ts[" << b+x-1 << "] <= lo2;\n";
-	std::cout << "\t\ts[" << b+x-2 << ":1] <= s[" << b+x-3 << ":0];\n";
-	std::cout << "\t\ts[0] <= lo1;\n";
-	std::cout << "\tend\n";
+	std::cout << "\tif (rst) out <= 0;\n";	
+	std::cout << "\telse out <= lo;\n";	
 	std::cout << "end\n";
 	std::cout << "endmodule\n";
-}
-
-int nextconfig (int i, bool s = 0)
-{
-	int j;
-	int c;
-	int target = s ? 3 : 4;
-	while (1)
-	{
-		i++;
-		i &= int(pow(2,b+x)-1);
-		c = 0;
-		for (j=0; j<b+x; j++)
-		{
-			c += (i >> j) & 1;
-		}
-		if (c == target) return i;
-	}
-}
-
-int nextreactor (int i, int mode)
-{
-	if (mode == 0) return int(pow(2,16)-1) & (i+1);
-	//if (mode == 1) return (i == 0)? 1 : int(pow(2,16)-1) & (i << 1);
-
-	// Else... Mode is the minimum number of zeroes or ones
-	// in the 16-bit reactor integer
-	int j;
-	int o;
-	while (1)
-	{
-		i++;
-		i &= int(pow(2,16)-1);
-		o = 0;
-		for (j=0; j<16; j++)
-		{
-			o += ((i >> j) & 1) ? 1 : 0;
-		}
-		if (o >= mode && 16-o >= mode) return i;
-	}
-	
-}
-
-inline int lut(int in, int data)
-{
-	return (data & (1 << in) ? 1 : 0);
-}
-
-int eval(bool reset, long int data, int config1, int config2)
-{
-	static int d = 0;
-	if (reset)
-	{
-		d = 0;
-		return 0;
-	}
-	int in1 = 0;
-	int in2 = 0;
-	int i;
-	int k = 0;
-	for (i=0;i<b+x;i++)
-	{
-		if ((config1 >> i) & 1) 
-		{
-			in1 += ((d >> i) & 1) << i-k;
-		}
-		else k++;
-	}
-	k = 0;
-	for (i=0;i<b+x;i++)
-	{
-		if ((config2 >> i) & 1) 
-		{
-			in2 += ((d >> i) & 1) << i-k;
-		}
-		else k++;
-	}
-	d = d << 1;
-	d = d & int(pow(2,b+x-1)-2);
-	d += lut(in1, data % int(pow(2,16)));
-	d += (lut(in2, (data >> 16)) << (b+x-1));
-	return d;
-}
-
-bool testloop(int mode , int config1, int config2, int mode1, int mode2)	// Returns 1 if succeeded
-{
-	// Mode 0 - only one reactor working
-	// Mode 1 - both reactors work with the same value
-	// Mode 2 - Brute force
-	long int i, j, k;
-	long int reactor1 = 0;
-	long int reactor2 = 0;
-	std::cout << "//// Test Loop " << ((mode) ? "with   " : "without") << " secondary LUT;\t";
-	std::cout << "Config1: " << bincout(config1, b+x) << "\t";
-	std::cout << "Config2: " << bincout(config2, b+x);
-	std::cout << "\tUseful b: " << b << "; Extra b: " << x << "\n";
-
-	while (reactor1 < nextreactor(reactor1, mode1))
-	//for (i=0;i<std::pow(2,range);i++)
-	{
-		if (mode == 0 || mode == 1) reactor1 = nextreactor(reactor1, mode1);
-		if (mode == 1) reactor2 = reactor1;
-		if (mode == 2)
-		{
-			if (reactor2 > nextreactor(reactor2,mode2)) 
-				reactor1 = nextreactor(reactor1, mode1);
-			reactor2 = nextreactor(reactor2,mode2);
-		}
-		i = reactor1 + (reactor2 << 16);
-
-		eval (1, 0, 0, 0); // reset
-		for (j=1; j<2*p; j++)				// This is the validity check
-		{
-			if (((eval(0, i, config1, config2) >> b+x-1) & 1) != ((j%p)>=(p/2))) goto next;
-		}
-		// At this point check had succeeded.
-		std::cout << "//// Found something!!\n";
-		std::cout << "//// Reactor1: " << bincout(reactor1,16);
-		std::cout << "\tReactor2: " << bincout(reactor2,16);
-		std::cout << "\ti: " << bincout(i,32);
-		std::cout << "\n";
-
-		j = eval(1, 0, 0, 0);		// reset
-		std::cout << "//// Output: " << j % max;
-		for (k=0; k<2*p+2; k++)
-		{
-			j = eval(0, i, config1, config2); 
-			std::cout << ", " << j;
-		}
-		printmodule(reactor1, reactor2, config1, config2, mode);
-		std::cout << "\n//// (BEL character) \a\n"; 		// BEL
-		return 1;
-next:		continue;
-	}	
-	return 0;
+	std::cout << std::flush;
 }
 
 int main(int argc, char** argv)
 {
 	if (argc < 3) {
-		std::cout << "Usage:\n";
-		std::cout << "prdiv [period] [extrabits]\n";
+		std::cerr << "Usage:\n";
+		std::cerr << "prdiv [period] [extrabits]\n";
 		return 0;
 	}
 	p = int(atof(argv[1]));
 	if (p > 10240) {
-		std::cout << p << "? Forget it..\n";
+		std::cerr << p << "? Forget it..\n";
 		return 0;
 	}
 	b = int(std::ceil(std::log2(p)));
 	if (b < 4) b = 4;
-	max = pow(2,b);
 	sx = int(atof(argv[2]));
-	
-	int config1 = 0;
-	int config2 = 0;
 
-	int i;
-	std::cout << "////>>> Brute forcing all possible LUT data combinations.\n";
+	srand(time(NULL));
+	
+	int ps = ((p+1)/2)-1;	// A number of selectable states (without two fixed states)
+	std::vector<int> states;
+
+	int gws = 0;		// Greatest working state
+	int tb = 0;		// Troublesome bit
+	int ltb = 0;		// Last troublesome bit
+
+	int i, j;
 	for (i=0; i<=sx; i++)
 	{
 		x = i;
-		config1 = 1;
-		config2 = 1;
-		if (i > 0) config1 = 1 << b+i-1;
+		//x = sx;
+		std::vector<comb> configs(b+x, comb(4, b+x));
+		std::vector<lut> luts(b+x);
 
-		while (config2 < nextconfig(config2, 0))
+		vari stv(1, std::pow(2,b+x-1)-1, ps);
+		long int cases = stv.cases();
+		long int cc = 0;
+		auto timer = std::chrono::high_resolution_clock::now() + std::chrono::seconds(10);
+		//while (mass_next(configs))
+		while (1)		// State list
 		{
-			config2 = nextconfig(config2, 0);
-			config1 = 1;
-			while (config1 < nextconfig(config1))
+			cc++;
+
+			if (timer < std::chrono::high_resolution_clock::now())
 			{
-				config1 = nextconfig(config1);
-				if (testloop(2, config1, config2, 0, 0)) return 0;
+				timer += std::chrono::seconds(10);
+				for (int j : states) std::cerr << j << " ";
+				std::cerr << "\t";
+				for (j=0; j<b+x; j++)
+				{
+					std::cerr << "C" << j << ":" << configs[j].str() << " ";
+				}
+				std::cerr << "\n";
 			}
+
+			states.clear();
+			states.push_back(0);
+			for (int j : stv.get()) states.push_back(j);
+			states.push_back(1 << b+x-1);
+			for (int j : stv.get()) states.push_back(j + (1 << b+x-1));
+			if (p%2) states.pop_back();
+
+			while (1)	// Config rollover
+			{
+				gws = 0;
+
+
+				int fl = fill_luts(luts, states, configs);
+				if (fl % (1 << 16) > gws) gws = fl % (1 << 16);
+
+				if (fl == 0)
+				{
+					// We have a winner!
+					std::cerr << "Found it! \n";
+					std::cerr << "States: ";
+					for (int j : states) std::cerr << j << " ";
+					std::cerr << ";\n";
+			
+					for (j=0; j<b+x; j++)
+					{
+						std::cerr << "LUT" << j << ": " << luts[j].str() << "\t";
+						std::cerr << "Config" << j << ": " << configs[j].str() << "\n";
+					}
+					printmodule(luts, states, configs);
+					return 0;
+				}
+
+				tb = fl/(1 << 16);
+				if (!configs[tb].next())
+				{
+					if (tb == ltb) break;
+					else ltb = tb;
+				}
+			}
+		if (stv.next(gws-1) == 0) break;
 		}
 	}
-		std::cout << "Found nothing :(\n";
-		std::cout << "\a"; 			// BEL
+		std::cerr << "Found nothing :(\n";
+		std::cerr << "\a"; 			// BEL
 		return 0;
 }
 
